f7a129ee067afa59537c349a06259547ac497085
Chen Liang <lchen81@sina.com>
2018-05-29 11:54:02 +0800

    change sqlite to mysql

--- zycloud/settings.py
+++ zycloud/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'zycloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zycloud',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'admin',
     }
 }
 
